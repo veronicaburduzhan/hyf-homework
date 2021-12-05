278b29e95e321b3619dff44fc1c0bf402cc3f259
Veronica Burduzhan <veronica.burduzhan@gmail.com>
2021-12-05 15:53:53 +0100

    Added files from JS week1 lesson

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,14 @@
+//part 1
+console.log("I love pizza");
+const myFavPizza = "Prosciutto di Parma"
+let myFavPizzaPrice = 50;
+console.log("New pizza order: " + myFavPizza + ". " + "The price of the pizza is: " + myFavPizzaPrice + ". ")
+
+//part 2
+let amountOfPizzas = 3;
+const sizeFamily = "Family size";
+const sizeRegular = "Regular size";
+let totalAmount = (myFavPizzaPrice * 2) * amountOfPizzas;
+console.log("New pizza order: " + amountOfPizzas + "pcs." + " " + sizeFamily + ". " + "Total cost for the order is: " + totalAmount + ". ");
+
+
