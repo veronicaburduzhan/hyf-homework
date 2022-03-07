1c900fb879bd78dbd3c9990e480b39b7eb3d1dc2
Veronica Burduzhan <veronica.burduzhan@gmail.com>
2022-03-07 21:44:26 +0100

    databases-week1

---
+++ databases/week1/class/homework.sql
@@ -0,0 +1,83 @@
+-- Find out how many tasks are in the task table
+SELECT 
+  COUNT(title) 
+FROM 
+  task;
+  
+-- Find out how many tasks in the task table do not have a valid due date
+SELECT 
+  COUNT(title) 
+FROM 
+  task 
+WHERE 
+  due_date IS NULL;
+
+-- Find all the tasks that are marked as done
+SELECT 
+  * 
+FROM 
+  task 
+  JOIN status ON task.status_id = status.id 
+WHERE 
+  status.name LIKE "%done%";
+
+-- Find all the tasks that are not marked as done
+SELECT 
+  * 
+FROM 
+  task 
+  JOIN status ON task.status_id = status.id 
+WHERE 
+  NOT status.name LIKE "%done%";
+
+-- Get all the tasks, sorted with the most recently created first
+SELECT 
+  * 
+FROM 
+  task 
+ORDER BY 
+  created DESC;
+
+-- Get the single most recently created task
+SELECT 
+  MAX(created) 
+FROM 
+  task;
+
+-- Get the title and due date of all tasks where the title or description contains database
+SELECT 
+  title, 
+  due_date 
+FROM 
+  task 
+WHERE 
+  title LIKE '%database%';
+
+-- Get the title and status (as text) of all tasks
+SELECT 
+  task.title, 
+  status.name 
+FROM 
+  task 
+  INNER JOIN status ON task.status_id = status.id;
+
+-- Get the name of each status, along with a count of how many tasks have that status
+SELECT 
+  COUNT(task.title), 
+  status.name 
+FROM 
+  task 
+  INNER JOIN status ON task.status_id = status.id 
+GROUP BY 
+  status.name;
+
+-- Get the names of all statuses, sorted by the status with most tasks first
+SELECT 
+  status.name 
+FROM 
+  task 
+  INNER JOIN status ON task.status_id = status.id 
+GROUP BY 
+  status.name 
+ORDER BY 
+  COUNT(task.title) DESC
