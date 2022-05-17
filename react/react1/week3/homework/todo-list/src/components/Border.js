92fe2324b9c824625a20581da8889111f6617952
Veronica Burduzhan <veronica.burduzhan@gmail.com>
2022-05-17 20:50:54 +0200

    Removed folder

--- react/react1/week3/homework/todo-list/src/components/Border.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Border(props) {
-    return <div className="borderStyle">{props.children}</div>
-};
--- react/react1/week3/homework/todo-list/src/components/TodoHeader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function TodoHeader() {
-  return (
-    <div className="todoHeader">
-      <h1>Todo List</h1>
-    </div>
-  );
-}
